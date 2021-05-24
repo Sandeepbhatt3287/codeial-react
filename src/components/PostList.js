56fb94a07b7c171f0531be5441c12f3862351d57
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-05-24 10:43:00 +0530

    Installing Axios

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,6 +3,11 @@ import React from 'react';
 // it will be class based 
 class PostList extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = 
+    }
+
 render(){
     return (
         <div>
